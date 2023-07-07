477077e9e6ed9a74b482dd015727f48547941bd1
Amanda Franklin <134338964+franklinamanda34@users.noreply.github.com>
2023-07-06 20:42:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// Assignment Code
+
+var generateBtn = document.querySelector("#generate");
+var length;
+var confirmnumber;
+var confirmlowercase;
+var confirmuppercase;
+
+number = (1,2,3,4,5,6,7,8,9)
+
+
+
+// Write password to the #password input
+function generatePassword() {
+  var password = document.generatePassword("#generate");
+  var passwordText = document.querySelector("#password");
+ 
+
+  passwordText.value = password;
+
+}
+
+
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
+
+if (number <1 but >10)
+
+
+
+
